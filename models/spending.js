a7b1ec6f28ebede569efd32ceffa51f178244e45
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-09-17 10:12:46 -0700

    Adding User and spending models

---
+++ models/spending.js
@@ -0,0 +1,14 @@
+// Require Mongoose node module
+const mongoose = require('mongoose');
+
+// Create Museum Schema
+const spendingSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  month: String,
+  category: String,
+  amount: Number,
+  description: String
+});
+
+// Use schema to create model and Export Museum Model
+module.exports = mongoose.model('Spending', spendingSchema);
