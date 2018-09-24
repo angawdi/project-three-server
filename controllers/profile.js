ce1a945cf4f60ebbd545bc1ada994b5c1f182351
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-09-23 21:39:06 -0700

    Merging

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const router = express.Router();
-// const db = require('../models');
-//
-// router.get('/', (req, res) => {
-// 	console.log("profile route!!")
-//    db.User.find()
-//  .then(info => {
-//    res.send(info);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     res.send(err);
-//   });
-// });
-//
-//
-// module.exports = router;
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+router.get('/', (req, res) => {
+	console.log("profile route!!")
+   db.User.find()
+ .then(info => {
+   res.send(info);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  });
+});
+
+
+module.exports = router;
--- controllers/users.js
+++ controllers/users.js
@@ -1,34 +1,34 @@
-// const express = require('express');
-// const router = express.Router();
-// const db = require('../models');
-//
-// router.get('/', (req, res) => {
-//   db.User.find()
-//   .then(users => {
-//       res.send(users);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     res.send(err);
-//   });
-// });
-//
-//
-// router.get('/createuser', (req, res) => {
-//   db.User.create({
-//     firstName: req.body.firstName,
-//     lastName: req.body.lastName,
-//     email: req.body.email,
-//     password: req.body.password,
-//   })
-//   .then(result => {
-//     res.send('success');
-//   })
-//   .catch(err => {
-//     console.log(err);
-//     res.send('error, check your logs');
-//   });
-// });
-//
-//
-// module.exports = router;
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+
+router.get('/', (req, res) => {
+  db.User.find()
+  .then(users => {
+      res.send(users);
+  })
+  .catch(err => {
+    console.log(err);
+    res.send(err);
+  });
+});
+
+
+router.get('/createuser', (req, res) => {
+  db.User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+  })
+  .then(result => {
+    res.send('success');
+  })
+  .catch(err => {
+    console.log(err);
+    res.send('error, check your logs');
+  });
+});
+
+
+module.exports = router;
